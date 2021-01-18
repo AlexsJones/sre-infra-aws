84c070d946e9a71ee2e6cdb29475bac80cc5e28b
Alex Jones <alexsimonjones@gmail.com>
2021-01-18 07:33:56 +0000

    wip

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,8 @@ data "aws_route53_zone" "base_domain" {
   name = var.dns_base_domain
 }
 
-# create AWS-issued SSL certificate
+data "aws_elb_hosted_zone_id" "elb_zone_id" {}
+
 resource "aws_acm_certificate" "eks_domain_cert" {
   domain_name               = var.dns_base_domain
   subject_alternative_names = ["*.${var.dns_base_domain}"]
@@ -12,16 +13,15 @@ resource "aws_acm_certificate" "eks_domain_cert" {
     Name            = "${var.dns_base_domain}"
   }
 }
-
-resource "aws_route53_record" "deployments_subdomains" {
-  for_each = toset(var.deployments_subdomains)
-  zone_id = data.aws_route53_zone.base_domain.id
-  name    = "${each.key}.${aws_route53_record.eks_domain.fqdn}"
-  type    = "CNAME"
-  ttl     = "5"
-  records = ["${data.kubernetes_service.ingress_gateway.load_balancer_ingress.0.hostname}"]
-}
-
+#resource "aws_route53_record" "deployments_subdomains" {
+#  for_each = toset(var.deployments_subdomains)
+#  zone_id = data.aws_route53_zone.base_domain.id
+#  name    = "${each.key}.${aws_route53_record.eks_domain.fqdn}"
+#  type    = "CNAME"
+#  ttl     = "5"
+#  records = ["${data.kubernetes_service.ingress_gateway.load_balancer_ingress.0.hostname}"]
+#}
+#
 resource "aws_route53_record" "eks_domain_cert_validation_dns" {
   for_each = {
     for dvo in aws_acm_certificate.eks_domain_cert.domain_validation_options : dvo.domain_name => {
@@ -41,26 +41,4 @@ resource "aws_route53_record" "eks_domain_cert_validation_dns" {
 resource "aws_acm_certificate_validation" "eks_domain_cert_validation" {
   certificate_arn         = aws_acm_certificate.eks_domain_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.eks_domain_cert_validation_dns : record.fqdn]
-}
-
-data "kubernetes_service" "ingress_gateway" {
-  metadata {
-    namespace = "kube-system"
-    name = join("-", [helm_release.ingress_gateway.chart, helm_release.ingress_gateway.name])
-  }
-  depends_on = [helm_release.ingress_gateway]
-}
-
-data "aws_elb_hosted_zone_id" "elb_zone_id" {}
-
-resource "aws_route53_record" "eks_domain" {
-  zone_id = data.aws_route53_zone.base_domain.id
-  name    = var.dns_base_domain
-  type    = "A"
-
-  alias {
-    name                   = data.kubernetes_service.ingress_gateway.load_balancer_ingress.0.hostname
-    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
-    evaluate_target_health = true
-  }
 }
---
+++ terraform/helm-charts.tf
@@ -0,0 +1,93 @@
+resource "helm_release" "spot_termination_handler" {
+  name      = "aws-node-termination-handler"
+  chart      = "aws-node-termination-handler"
+  repository      = "https://aws.github.io/eks-charts"
+  version   = "0.9.1"
+  namespace = "kube-system"
+}
+############################################################
+resource "helm_release" "gitlab" {
+  name = "gitlab"
+  chart = "gitlab"
+  repository = "https://charts.gitlab.io/"
+  namespace = "gitlab"
+  version = "4.7.4"
+  create_namespace = true
+
+  set {
+    name = "nginx-ingress.enabled"
+    value = true
+  }
+  set {
+    name = "global.hosts.domain"
+    value = var.dns_base_domain
+  }
+  set {
+    name = "global.edition"
+    value = "ce"
+  }
+  set {
+    name = "certmanager-issuer.email"
+    value = var.certmanager_email
+  }
+}
+
+data "kubernetes_ingress" "gitlab-web" {
+  metadata {
+    name = "gitlab-webservice-default"
+    namespace = "gitlab"
+  }
+}
+
+resource "aws_route53_record" "gitlab_subdomain" {
+  zone_id = data.aws_route53_zone.base_domain.id
+  name    = join(".",["gitlab",var.dns_base_domain])
+  type    = "CNAME"
+
+  alias {
+    name =  data.kubernetes_ingress.gitlab-web.load_balancer_ingress[0].hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "gitlab_minio_subdomain" {
+  zone_id = data.aws_route53_zone.base_domain.id
+  name    = join(".",["minio",var.dns_base_domain])
+  type    = "CNAME"
+
+  alias {
+    name =  data.kubernetes_ingress.gitlab-web.load_balancer_ingress[0].hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+  }
+}
+resource "aws_route53_record" "gitlab_registry_subdomain" {
+  zone_id = data.aws_route53_zone.base_domain.id
+  name    = join(".",["registry",var.dns_base_domain])
+  type    = "CNAME"
+
+  alias {
+    name =  data.kubernetes_ingress.gitlab-web.load_balancer_ingress[0].hostname
+    zone_id                = data.aws_elb_hosted_zone_id.elb_zone_id.id
+    evaluate_target_health = true
+  }
+}
+############################################################
+#resource "helm_release" "ingress" {
+#  name      = "ingress"
+#  chart      = "aws-alb-ingress-controller"
+#  repository      = "https://charts.helm.sh/incubator"
+#  namespace = "kube-system"
+#  set {
+#    name = "autoDiscoverAwsRegion"
+#    value = true
+#  }
+#  set {
+#    name = "autoDiscoverAwsVpcID"
+#    value = true
+#  }
+#    set {
+#    name  = "clusterName"
+#    value = var.cluster_name
+#  }
+#}
