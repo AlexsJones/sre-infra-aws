355d89e386ebe825e6b1b890781295865c4fc65a
Alex Jones <alexsimonjones@gmail.com>
2021-01-21 21:41:02 +0000

    chore(updated): r53 backend

--- terraform/helm_release_ingress.tf
+++ terraform/helm_release_ingress.tf
@@ -17,7 +17,6 @@ variable "ingress_gateway_annotations" {
   type = map(string)
   default = {
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"        = "http",
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"               = "https",
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout" = "60",
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"                    = "elb"
   }
