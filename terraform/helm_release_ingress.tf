d40cc7eb1dcda1245d03eb38c03b722793553203
Alex Jones <alexsimonjones@gmail.com>
2021-01-21 15:04:49 +0000

    chore(updated): elb

--- terraform/helm_release_ingress.tf
+++ terraform/helm_release_ingress.tf
@@ -1,37 +1,34 @@
 variable "ingress_gateway_chart_name" {
-  type = string
+  type    = string
   default = "nginx-ingress"
 }
 
 variable "ingress_gateway_chart_repo" {
-  type = string
+  type    = string
   default = "https://helm.nginx.com/stable"
 }
 
 variable "ingress_gateway_chart_version" {
-  type = string
+  type    = string
   default = "0.5.2"
 }
 
 variable "ingress_gateway_annotations" {
   type = map(string)
   default = {
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"        = "http"
-    "controller.service.httpPort.targetPort"                                                                    = "http",
-    "controller.service.httpsPort.targetPort"                                                                   = "http",
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-ssl-ports"               = "https",
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-cross-zone-load-balancing-enabled" = "true",
+    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-backend-protocol"        = "http",
     "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-connection-idle-timeout" = "60",
-    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"                    = "nlb"
+    "controller.service.annotations.service\\.beta\\.kubernetes\\.io/aws-load-balancer-type"                    = "elb"
   }
 }
 
 resource "helm_release" "ingress_gateway" {
-  name       = var.ingress_gateway_chart_name
-  chart      = var.ingress_gateway_chart_name
-  repository = var.ingress_gateway_chart_repo
-  version    = var.ingress_gateway_chart_version
-
+  name             = var.ingress_gateway_chart_name
+  namespace        = "gateway"
+  chart            = var.ingress_gateway_chart_name
+  repository       = var.ingress_gateway_chart_repo
+  version          = var.ingress_gateway_chart_version
+  create_namespace = true
   dynamic "set" {
     for_each = var.ingress_gateway_annotations
 
@@ -48,17 +45,17 @@ resource "helm_release" "ingress_gateway" {
   }
 }
 
-# create base domain for EKS Cluster
 data "kubernetes_service" "ingress_gateway" {
   metadata {
-    name = join("-", [helm_release.ingress_gateway.chart, helm_release.ingress_gateway.name])
+    name      = join("-", [helm_release.ingress_gateway.chart, helm_release.ingress_gateway.name])
+    namespace = "gateway"
   }
 
 }
 
 resource "aws_route53_record" "eks_domain" {
   zone_id = data.aws_route53_zone.base_domain.id
-  name = var.dns_base_domain
+  name    = var.dns_base_domain
   type    = "A"
 
   alias {
